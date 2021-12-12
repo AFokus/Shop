92a2706349857933886a73c46c314e08b3cf4a57
Yaroslav Prorubschikov <y.prorubschikov@furylion.dev>
2021-12-13 02:58:48 +0300

    DOTween added

---
+++ WebShop/Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void LoadAll()
+    {
+        
+    }
+
+    public void Open<T>() where T : View
+    {
+        
+    }
+
+    public void CLose<T>() where T : View
+    {
+        
+    }
+}
---
+++ WebShop/Assets/Scripts/View.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class View : MonoBehaviour
+{
+    public virtual void OnCreate()
+    {
+        
+    }
+
+    public virtual void OnOpen()
+    {
+        
+    }
+    
+    public virtual void OnClose()
+    {
+        
+    }
+}
